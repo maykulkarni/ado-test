6dac907405e758d6642980c93a06e0569eace422
Mayur Kularni <mayurkulkarni012@gmail.com>
2020-01-06 17:58:59 +0530

    asdf

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setuptools.setup(
 		version=1.0,
 		description="Foo Bar",
 		author="AzSK Dev Team",
-		author_email="mayurk",
+		author_email="mayurkulkarni012@gmail.com",
 		url="https://127.0.0.1",
 		packages=setuptools.find_packages(),
 		install_requires=[
